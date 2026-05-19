4f2c746bf500cab04cc447966d64ab290b2e1086
kvlwex <118870326+kvlwex@users.noreply.github.com>
2026-05-19 05:22:28 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,1 +1,12 @@
+from database import connect_db
 
+
+def test_connection():
+    connection = connect_db()
+
+    assert connection is not None
+
+    print("Тест подключения успешно пройден")
+
+
+test_connection()
