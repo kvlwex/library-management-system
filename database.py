8a3429a67f1c0103f3129281d6ebc9d174f56ba9
kvlwex <118870326+kvlwex@users.noreply.github.com>
2026-05-19 05:21:56 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,34 @@
+import psycopg2
 
+
+def connect_db():
+    try:
+        connection = psycopg2.connect(
+            database="library_db",
+            user="postgres",
+            password="postgres",
+            host="localhost",
+            port="5432"
+        )
+
+        return connection
+
+    except Exception as error:
+        print("Ошибка:", error)
+        return None
+
+
+def get_books():
+    connection = connect_db()
+
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM books"
+
+    cursor.execute(query)
+
+    books = cursor.fetchall()
+
+    connection.close()
+
+    return books
