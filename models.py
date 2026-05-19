3e32abdb0a7715634de1e4254c75f7016534d7a8
kvlwex <118870326+kvlwex@users.noreply.github.com>
2026-05-19 05:22:15 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,17 @@
 
+class Book:
+    def __init__(self, title, year):
+        self.title = title
+        self.year = year
+
+
+class Reader:
+    def __init__(self, full_name, phone):
+        self.full_name = full_name
+        self.phone = phone
+
+
+class Loan:
+    def __init__(self, book_id, reader_id):
+        self.book_id = book_id
+        self.reader_id = reader_id
