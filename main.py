6de30199ec3eee8a904244cf431e7e909b8f4a4b
kvlwex <118870326+kvlwex@users.noreply.github.com>
2026-05-19 05:21:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+from database import connect_db, get_books
 
+def main():
+    print("Система управления библиотекой")
+
+    connection = connect_db()
+
+    if connection:
+        print("Подключение успешно")
+
+        books = get_books()
+
+        print("\nСписок книг:")
+
+        for book in books:
+            print(book)
+
+        connection.close()
+
+    else:
+        print("Ошибка подключения")
+
+
+if __name__ == "__main__":
+    main()
