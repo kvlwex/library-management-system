469a52701443573f67b006ec49d6993c7837b5fb
kvlwex <118870326+kvlwex@users.noreply.github.com>
2026-05-19 05:22:38 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,12 @@
+CREATE TABLE books (
+    book_id SERIAL PRIMARY KEY,
+    title VARCHAR(100),
+    publish_year INTEGER
+);
 
+INSERT INTO books(title, publish_year)
+VALUES
+('Война и мир', 1869),
+('Преступление и наказание', 1866);
+
+SELECT * FROM books;
